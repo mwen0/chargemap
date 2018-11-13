4a23cfc0da66ccaa47668e7bdce5b1b0a3d61c6c
Raghu Betina <raghu@firstdraft.com>
2018-11-13 16:54:22 +0000

    Add forms for new collection objects on show pages

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -40,6 +40,29 @@ class AddressesController < ApplicationController
     end
   end
 
+  def create_row_from_country
+    @address = Address.new
+
+    @address.latitude = params.fetch("latitude")
+    @address.longitude = params.fetch("longitude")
+    @address.name = params.fetch("name")
+    @address.address_line1 = params.fetch("address_line1")
+    @address.address_line2 = params.fetch("address_line2")
+    @address.town = params.fetch("town")
+    @address.post_code = params.fetch("post_code")
+    @address.state_or_province = params.fetch("state_or_province")
+    @address.has_ev_parking_places = params.fetch("has_ev_parking_places")
+    @address.country_id = params.fetch("country_id")
+
+    if @address.valid?
+      @address.save
+
+      redirect_to("/countries/#{@address.country_id}", notice: "Address created successfully.")
+    else
+      render("address_templates/new_form_with_errors.html.erb")
+    end
+  end
+
   def edit_form
     @address = Address.find(params.fetch("prefill_with_id"))
 
