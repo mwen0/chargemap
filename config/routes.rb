f54a2bded9db6628c4d0c7370b803cdd8a39a03d
Raghu Betina <raghu@firstdraft.com>
2018-11-13 16:54:04 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "people#index"
   # Routes for the Address resource:
 
